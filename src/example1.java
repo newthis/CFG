5b2d87e5d4a8a6df6e45a17d2036a226e17fd99f
Ali Alotaibi <alisotaibi@gmail.com>
2018-10-16 14:45:42 -0700

    .

--- src/example1.java
+++ src/example1.java
@@ -1,11 +1,15 @@
 public class example1 {
 	public static void main(String[] args) {
 		int a = 0;
-		if (6 == a) {
+		for (int i = 0; i < a; i++) {
+			
+		if (6 == a&& a==5) {
 			a = 6;
 		}else if (a == 8) {
 			System.out.println("x");
 		}
-
+		a=20;
+	}
 	}
 }
+
